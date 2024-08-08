8426cc5f168951f328fe947b45a5b03976aab1ea
Runnin4ik <mr.kirilll12@mail.ru>
2024-08-08 23:33:02 +0300

    update version

--- src/config_simplevk.php
+++ src/config_simplevk.php
@@ -2,7 +2,7 @@
 
 namespace DigitalStars\SimpleVK;
 
-const SIMPLEVK_VERSION = '3.1.7';
+const SIMPLEVK_VERSION = '3.1.8';
 // массив кодов ошибок ВК, при которых идет 5 попыток выполнить этот запрос с перерывом в 10 секунд
 const ERROR_CODES_FOR_MANY_TRY = [
     1, //Произошла неизвестная ошибка.
