6bdede98c7af2bddb691e1e5f58d4361f3c29868
Runnin4ik <mr.kirilll12@mail.ru>
2024-11-11 18:11:44 +0300

    Доработка ErrorHandler.php - Цветовое выделение трейса в консоли - Метод shortTrace() для того, чтобы убрать из трейса файлы либы - Метод setTracePathFilter() для того, чтобы сократить пути файлов в трейсе - Ошибки типо Warning и подобные теперь имеют полный трейс

--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -7,9 +7,10 @@ require_once('config_simplevk.php');
 trait ErrorHandler {
 
     private mixed $user_error_handler_or_ids = null;
-    private bool $is_exception_printed = false;
 
     private array $paths_to_filter = [];
+
+    private bool $short_trace = false;
 
     /**
      * Устанавливает обработчик ошибок и исключений, перенаправляя их для логирования и вывода.
@@ -44,6 +45,79 @@ trait ErrorHandler {
         $this->paths_to_filter = array_map(static fn($path) => str_replace('\\', '/', $path), $pathes);
     }
 
+    /**
+     * Оставляет в трейсе только пользовательские файлы, без файлов библиотеки
+     * @param bool $enable - вкл/выкл отображение короткого трейса
+     * @return self Возвращает текущий экземпляр для цепочки вызовов.
+     */
+    public function shortTrace(bool $enable = true): self {
+        $this->short_trace = $enable;
+        return $this;
+    }
+
+    /**
+     * Публичный, потому что исключения могут вызываться и обрабатываться за пределами текущего класса
+     */
+    public function exceptionHandler(\Throwable $exception, int $set_type = E_ERROR, bool $need_skip_check_error_without_trace = false): void {
+        $message = $this->normalizeMessage($exception->getMessage());
+        $message = $this->filterPaths($message);
+        $message = $this->coloredLog($message, 'RED');
+        $file = $this->normalizeMessage($exception->getFile());
+        $line = $this->normalizeMessage($exception->getLine());
+        $code = $exception->getCode();
+        $trace = $this->buildNewTrace($exception->getTrace(), $file, $line, $need_skip_check_error_without_trace);
+
+        $this->userErrorHandler($set_type, $message . "\n\n\n$trace", $file, $line, $code, $exception, $need_skip_check_error_without_trace);
+    }
+
+    /**
+     * Публичный, потому что исключения могут вызываться и обрабатываться за пределами текущего класса
+     */
+    public function userErrorHandler(int $type, string $message, string $file, int $line, ?int $code = null, ?\Throwable $exception = null, bool $need_skip_check_error_without_trace = false): void {
+        // если ошибка попадает в отчет (при использовании оператора "@" error_reporting() вернет 0)
+        if (error_reporting() & $type) {
+            $error_type_without_trace = [
+                E_WARNING,   // Системные предупреждения
+                E_NOTICE,    // Системные уведомления
+                E_USER_WARNING,   // Пользовательские предупреждения
+                E_USER_NOTICE,    // Пользовательские уведомления
+                E_USER_ERROR,     // Пользовательские ошибки
+                E_DEPRECATED,     // Устаревшие функции
+                E_USER_DEPRECATED // Пользовательские устаревшие функции
+            ];
+
+            if (!$need_skip_check_error_without_trace && in_array($type, $error_type_without_trace, true)) {
+                 //Создаем исключение для типа ошибки, не содержащей полного трейса
+                $exception = new SimpleVkException(0, $message);
+                $this->exceptionHandler($exception, $type, true); // Передаем оригинальный тип ошибки и получаем полный трейс
+                return;
+            }
+
+            [$error_level, $error_type] = $this->defaultErrorLevelMap()[$type];
+            $error_level_str = $this->formatErrorLevel($error_level);
+
+            $msg = "{$error_level_str}{$message}";
+            $msg = str_replace("\n\n", "\n", $msg);
+            $msg_for_vk = preg_replace('/\033\[[0-9;]*m/', '', $msg); //Очистка от цвета
+
+            $this->dispatchErrorMessage($error_type, $msg_for_vk, $code, $exception);
+
+            if ($exception) {
+                print $msg;
+            }
+        }
+    }
+
+    private function checkForFatalError(): void {
+        if ($error = error_get_last()) {
+            $type = $error['type'];
+            if ($type & DEFAULT_ERROR_LOG) {
+                //запускаем обработчик ошибок
+                $this->userErrorHandler($type, $error['message'], $error['file'], $error['line']);
+            }
+        }
+    }
+
     private function defaultErrorLevelMap(): array {
         return [
             E_ERROR => ['CRITICAL', 'E_ERROR'],
@@ -64,32 +138,25 @@ trait ErrorHandler {
         ];
     }
 
-    private function userErrorHandler(int $type, string $message, string $file, int $line, ?int $code = null, ?\Throwable $exception = null): void {
-        // если ошибка попадает в отчет (при использовании оператора "@" error_reporting() вернет 0)
-        if (error_reporting() & $type) {
-            [$error_level, $error_type] = $this->defaultErrorLevelMap()[$type];
-            $error_level_str = $this->formatErrorLevel($error_level);
-
-            $msg = $this->is_exception_printed
-                ? "$error_level_str $message"
-                : "$error_level_str $message ($file на $line строке)\n➡" . $this->getCodeSnippet($file, $line);
-
-            $this->is_exception_printed = false;
-            $this->dispatchErrorMessage($error_type, $msg, $code, $exception);
-
-            if ($exception) {
-                print $msg;
-            }
-        }
-    }
-
     private function formatErrorLevel(string $level): string {
         return match ($level) {
-            'ERROR', 'CRITICAL' => '‼Fatal Error: ',
-            'WARNING' => '⚠️Warning: ',
-            'NOTICE' => '⚠️Notice: ',
-            default => '‼Unknown Error: ',
+            'ERROR', 'CRITICAL' => $this->coloredLog('‼Fatal Error: ', 'RED'),
+            'WARNING' => $this->coloredLog('⚠️Warning: ', 'YELLOW'),
+            'NOTICE' => $this->coloredLog('⚠️Notice: ', 'BLUE'),
+            default => $this->coloredLog('‼Unknown Error: ', 'RED'),
         };
+    }
+
+    private function coloredLog(string $text, string $color) {
+        $color_codes = [
+            'RED'     => "\033[31m",
+            'GREEN'   => "\033[32m",
+            'YELLOW'  => "\033[33m",
+            'BLUE'    => "\033[34m",
+            'WHITE'   => "\033[37m",
+        ];
+        $color_code = $color_codes[$color];
+        return "{$color_code}{$text}\033[0m";
     }
 
     private function dispatchErrorMessage(string $type, string $message, ?int $code = null, ?\Throwable $exception = null): void {
@@ -106,17 +173,7 @@ trait ErrorHandler {
         }
     }
 
-    private function checkForFatalError(): void {
-        if ($error = error_get_last()) {
-            $type = $error['type'];
-            if ($type & DEFAULT_ERROR_LOG) {
-                //запускаем обработчик ошибок
-                $this->userErrorHandler($type, $error['message'], $error['file'], $error['line']);
-            }
-        }
-    }
-
-    private function getCodeSnippet(string $file, int $line, int $padding = 0): string {
+    protected function getCodeSnippet(string $file, int $line, int $padding = 0): string {
         static $files_cache = [];
 
         if (!isset($files_cache[$file]) && is_readable($file)) {
@@ -133,7 +190,10 @@ trait ErrorHandler {
         $snippet = '';
 
         for ($i = $start; $i < $end; $i++) {
-            $snippet .= ($i + 1) . ': ' . trim($lines[$i]) . PHP_EOL;
+            $line_number = ($i + 1) . ': ';
+            $line_number = $this->coloredLog($line_number, 'YELLOW');
+            $code = $this->coloredLog(trim($lines[$i]), 'WHITE');
+            $snippet .= $line_number . $code . PHP_EOL;
         }
 
         return $snippet;
@@ -148,25 +208,15 @@ trait ErrorHandler {
             static function ($search) {
                 $indent_size = (int) ceil((mb_strlen($search[0]) - 1) / 2);
                 $indent = str_repeat("&#8199;", $indent_size);
-                return "\n&#8288;" . $indent;
+                return "\n" . $indent;
             },
             trim($message)
         );
     }
 
-    public function exceptionHandler(\Throwable $exception): void {
-        $this->is_exception_printed = true;
-        $message = $this->normalizeMessage($exception->getMessage());
-        $file = $this->normalizeMessage($exception->getFile());
-        $line = $this->normalizeMessage($exception->getLine());
-        $code = $exception->getCode();
-        $trace = $this->buildNewTrace($exception->getTrace(), $file, $line);
-
-        $this->userErrorHandler(E_ERROR, $message . "\n\n$trace", $file, $line, $code, $exception);
-    }
-
-    private function buildNewTrace(array $traceData, string $file, int $line): string {
-        $trace = $this->formatTraceLine(['file' => $file, 'line' => $line], 0);
+    private function buildNewTrace(array $traceData, string $file, int $line, bool $need_skip_check_error_without_trace): string {
+        //если нет трейса, значит мы получили его искуственно и первый элемент трейса не нужен
+        $trace = $need_skip_check_error_without_trace ? '' : $this->formatTraceLine(['file' => $file, 'line' => $line], 0);
 
         foreach ($traceData as $num => $data) {
             $trace .= $this->formatTraceLine($data, $num + 1);
@@ -189,12 +239,22 @@ trait ErrorHandler {
 
         $code_snippet = $this->getCodeSnippet($file, (int)$line);
         $pattern = '#/(simplevk3/src|vendor|simplevk-master/src|simplevk-testing/src)(/.*)#';
+        $user_file_marker = '➡ ';
 
-        if (preg_match($pattern, $formatted_file, $matches)) { //если файл либы, то не ставим ➡
+        if (preg_match($pattern, $formatted_file, $matches) || !isset($trace['file'])) {
             $formatted_file = ".." . $matches[0];
-            $trace_line .= "#{$num} " . $formatted_file . "($line)\n{$code_snippet}\n\n";
+            $user_file_marker = '';
+            if(!$this->short_trace) {
+                $trace_line .= $this->coloredLog("{$user_file_marker}#{$num} ", 'GREEN')
+                    . $this->coloredLog($formatted_file, 'BLUE')
+                    . $this->coloredLog("($line)", 'YELLOW') . "\n"
+                    . $this->coloredLog($code_snippet, 'WHITE') . "\n\n";
+            }
         } else {
-            $trace_line .= "➡ #{$num} " . $formatted_file . "($line)\n{$code_snippet}\n\n";
+            $trace_line .= $this->coloredLog("{$user_file_marker}#{$num} ", 'GREEN')
+                . $this->coloredLog($formatted_file, 'BLUE')
+                . $this->coloredLog("($line)", 'YELLOW') . "\n"
+                . $this->coloredLog($code_snippet, 'WHITE') . "\n\n";
         }
 
         return $trace_line;
