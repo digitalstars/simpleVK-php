db65999959713e8c332074f197a086872ecf2657
Runnin4ik <mr.kirilll12@mail.ru>
2024-11-12 16:52:31 +0300

    Переработка SimpleVkException.php - Исправлена проблема, когда директория с ошибками создавалась внутри исходников библиотеки. Теперь в случае неявного указания пути до папки ошибок, папка будет создаваться рядом с корневым запускаемым файлом. - Удален метод userError() - nullError() переименован в logCustomError - Изменен формат даты в названии логов, теперь сортировка в папке будет нормальной. - Улучшения ситуаций с созданием папок и файлов - Рефакторинг

--- src/SimpleVkException.php
+++ src/SimpleVkException.php
@@ -12,71 +12,96 @@ class SimpleVkException extends Exception {
      * Логирование ошибок в файл
      * @var true
      */
-    private static $write_error = true;
+    private static bool $write_error = true;
+    private static string $error_dir_path = '';
 
-    public function __construct($code, $message, Throwable $previous = null) {
-        if(self::$write_error === true) {
-            if (!is_dir(DIRNAME . '/error')) {
-                if (mkdir($concurrentDirectory = DIRNAME . '/error') || !is_dir($concurrentDirectory)) {
-                    $this->printError($code, $message);
-                }
-            } else
-                $this->printError($code, $message);
+    public function __construct(int $code, string $message, Throwable $previous = null) {
+        if (self::$write_error) {
+            self::ensureErrorDirectoryExists();
+            $this->logError($code, $message);
         }
-
         parent::__construct($message, $code, $previous);
     }
 
-    public static function userError(SimpleVkException $e) {
-        $error = "[Exception] " . date("d.m.y H:i:s");
-        $error .= "\r\n{$e->getMessage()}";
-        $error .= "\r\nin: {$e->getFile()}:{$e->getLine()}";
-        $error .= "\r\nStack trace:\r\n{$e->getTraceAsString()}\r\n\r\n";
-        return $error;
-    }
-
-    public static function nullError($message) {
-        if (!is_dir('error')) {
-            if (mkdir('error') || is_dir('error')) {
-                self::writeToLog($message);
-            }
-        } else
-            self::writeToLog($message);
-    }
-
-    private function printError($code, $message) {
-        $error = "[Exception] " . date("d.m.y H:i:s");
-        $error .= "\r\nCODE: {$code}";
-        $error .= "\r\nMESSAGE: {$message}";
-        $error .= "\r\nin: {$this->getFile()}:{$this->getLine()}";
-        $error .= "\r\nStack trace:\r\n{$this->getTraceAsString()}\r\n\r\n";
-        $path = DIRNAME . '/error/error_log' . date('d-m-Y') . ".php";
-        SimpleVkException::createNewLogFile($path);
-        $file = fopen($path, 'a');
-        fwrite($file, $error);
-        fclose($file);
-    }
-
-    private static function writeToLog($message) {
-        $error = "[Exception] " . date("d.m.y H:i:s");
-        $error .= "\r\nMESSAGE: {$message}\r\n\r\n";
-        $path = DIRNAME . '/error/error_log' . date('d-m-Y_h') . ".php";
-        SimpleVkException::createNewLogFile($path);
-        $file = fopen($path, 'a');
-        fwrite($file, $error);
-        fclose($file);
-    }
-
-    private static function createNewLogFile($path) {
-        if (!file_exists($path))
-            file_put_contents($path, "<?php http_response_code(404);exit(\"404\");?>\r\nLOGS:\r\n\r\n");
-    }
-
     /**
-     * Выключить логирование ошибок в файл
+     * Выключить логирование ошибок в файл.
      * По умолчанию ошибки записываются
+     * @return void
      */
     public static function disableWriteError(){
         self::$write_error = false;
+    }
+
+    /**
+     * Устанавливает относительный или абсолютный путь до папки, куда будут логироваться ошибки
+     * @param string $path
+     * @return void
+     */
+    public static function setErrorDirPath(string $path): void {
+        $real_path = realpath($path);
+        if (!$real_path) {
+            self::ensureErrorDirectoryExists($path);
+            $real_path = realpath($path);
+        }
+
+        self::$error_dir_path = $real_path;
+    }
+
+    public static function logCustomError(string $message): void {
+        self::ensureErrorDirectoryExists();
+        self::writeToLog($message);
+    }
+
+    private function logError(int $code, string $message): void {
+        $error_message = sprintf(
+            "[Exception] %s\nCODE: %d\nMESSAGE: %s\nin: %s:%d\nStack trace:\n%s\n\n",
+            date("d.m.y H:i:s"),
+            $code,
+            $message,
+            $this->getFile(),
+            $this->getLine(),
+            $this->getTraceAsString()
+        );
+        self::writeToLogFile($error_message);
+    }
+
+    private static function writeToLog(string $message): void {
+        $error_message = sprintf(
+            "[Exception] %s\nMESSAGE: %s\n\n",
+            date("d.m.y H:i:s"),
+            $message
+        );
+        self::writeToLogFile($error_message);
+    }
+
+    private static function writeToLogFile(string $message): void {
+        $path = self::$error_dir_path . "/error_log" . date('Y-m-d') . ".php";
+        self::createLogFileIfNotExists($path);
+        file_put_contents($path, $message, FILE_APPEND | LOCK_EX);
+    }
+
+    private static function createLogFileIfNotExists(string $path): void {
+        if (!file_exists($path)) {
+            file_put_contents(
+                $path,
+                "<?php http_response_code(404);exit(\"404\");?>\nLOGS:\n\n",
+                LOCK_EX
+            );
+        }
+    }
+
+    /**
+     * Проверка и создание папки error, если она отсутствует
+     */
+    private static function ensureErrorDirectoryExists(?string $path = null): void {
+        if(!self::$error_dir_path) {
+            self::$error_dir_path = getcwd() . DIRECTORY_SEPARATOR. 'error';
+        }
+        $path = $path ?: self::$error_dir_path;
+        if (!is_dir($path)) {
+            if (!mkdir($path, 0755, true) && !is_dir($path)) {
+                throw new \RuntimeException(sprintf('Не удалось создать директорию для ошибок по пути %s', $path));
+            }
+        }
     }
 }
