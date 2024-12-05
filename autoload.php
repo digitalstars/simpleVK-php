09d5bffa593df087a835b56f095a15c70b05fedd
Runnin4ik <mr.kirilll12@mail.ru>
2024-12-06 00:16:41 +0300

    Фикс кастомного autoload.php

--- autoload.php
+++ autoload.php
@@ -8,6 +8,7 @@ spl_autoload_register(static function ($class) {
     if (str_starts_with($class, 'DigitalStars\\SimpleVK')) {
         $baseDir = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
         $class = str_replace('DigitalStars\\SimpleVK\\', '', $class);
+        $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
         $file = $baseDir . "$class.php";
         if (file_exists($file)) {
             require $file;
