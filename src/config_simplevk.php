11128fb3fd60e5fc18ab9eaed23d51d94af873a2
Runnin4ik <mr.kirilll12@mail.ru>
2025-03-24 01:47:35 +0300

    v3.1.25-beta

--- src/config_simplevk.php
+++ src/config_simplevk.php
@@ -2,7 +2,7 @@
 
 namespace DigitalStars\SimpleVK;
 
-const SIMPLEVK_VERSION = '3.1.24-beta';
+const SIMPLEVK_VERSION = '3.1.25-beta';
 // массив кодов ошибок ВК, при которых идет 5 попыток выполнить этот запрос с перерывом в 10 секунд
 const ERROR_CODES_FOR_MANY_TRY = [
     1, //Произошла неизвестная ошибка.
