72d7efb0e2aeea81f6fb5d0fcf458e5393cdaaa5
Runnin4ik <mr.kirilll12@mail.ru>
2025-07-25 17:20:12 +0300

    v3.8.3

--- src/config_simplevk.php
+++ src/config_simplevk.php
@@ -2,7 +2,7 @@
 
 namespace DigitalStars\SimpleVK;
 
-const SIMPLEVK_VERSION = '3.8.2';
+const SIMPLEVK_VERSION = '3.8.3';
 // массив кодов ошибок ВК, при которых идет 5 попыток выполнить этот запрос с перерывом в 10 секунд
 const ERROR_CODES_FOR_MANY_TRY = [
     1, //Произошла неизвестная ошибка.
